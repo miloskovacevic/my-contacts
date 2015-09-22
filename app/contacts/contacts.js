55a0b2164db3dae4295fa9fe26053af934773e04
Milos Kovacevic <miloscpt@gmail.com>
2015-09-23 00:55:08 +0200

    Form, firebase, angfire setup

---
+++ app/contacts/contacts.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('myContacts.contacts', ['ngRoute','firebase'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/contacts', {
+    templateUrl: 'contacts/contacts.html',
+    controller: 'ContactsCtrl'
+  });
+}])
+
+.controller('ContactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+  var ref = new Firebase("https://my-contacts-w.firebaseio.com/contacts");
+  $scope.contacts = $firebaseArray(ref);
+      //console.log($scope.contacts);
+
+  $scope.showAddForm = function(){
+      $scope.addFormShow = true;
+  }
+    $scope.cancel = function(){
+        $scope.addFormShow = false;
+    }
+
+
+}]);
